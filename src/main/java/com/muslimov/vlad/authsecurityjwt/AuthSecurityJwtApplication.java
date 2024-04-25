fb273d07fa2e26107cc6725fa67c1b9dd1918766
Vlad Muslimov <muslimov.vlad@mail.ru>
2024-04-25 09:27:59 +0500

    Initial commit

---
+++ src/main/java/com/muslimov/vlad/authsecurityjwt/AuthSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.muslimov.vlad.authsecurityjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthSecurityJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthSecurityJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muslimov/vlad/authsecurityjwt/AuthSecurityJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muslimov.vlad.authsecurityjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSecurityJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
